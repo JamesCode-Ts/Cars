77f49b79b4b99b82a208e3c09b8eec29779a392b
jamesCode-Ts <jameslob42@gmail.com>
2024-04-08 18:42:55 -0300

    first commit

---
+++ src/test/java/br/com/automobiles/cars/AutomobilesOfCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.automobiles.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomobilesOfCarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
